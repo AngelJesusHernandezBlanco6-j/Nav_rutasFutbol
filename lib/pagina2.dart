e368637bf09099ad3e7f94782e58757a173ca64c
AngelJesusHernandezBlanco6-j <99343367+AngelJesusHernandezBlanco6-j@users.noreply.github.com>
2022-04-03 01:24:45 -0600

    Update pagina2.dart

--- lib/pagina2.dart
+++ lib/pagina2.dart
@@ -7,67 +7,96 @@ class Pantalla2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('segunda Screen'),
+        title: const Text('Segunda pantalla'),
       ),
-      body: Center(
-        children: [
-            Card(
-              child: const ListTile(
-                leading: Icon(Icons.people),
-                title: Text(
-                  "Medicamentos.com",
-                  style: TextStyle(color: Colors.indigo),
-                ),
-              ),
-              elevation: 8,
-              shadowColor: Colors.red,
-              margin: const EdgeInsets.all(20),
-              shape: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Colors.red, width: 2)),
-            ), //Card1
-            Card(
-              child: const ListTile(
-                leading: Icon(Icons.people),
-                title: Text(
-                  "Empleados.com",
-                  style: TextStyle(color: Colors.indigo),
-                ),
-              ),
-              elevation: 8,
-              shadowColor: Colors.red,
-              margin: const EdgeInsets.all(20),
-              shape: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Colors.red, width: 2)),
-            ), //Card2
-             Card(
-              child: const ListTile(
-                leading: Icon(Icons.people),
-                title: Text(
-                  "Clientes.com",
-                  style: TextStyle(color: Colors.indigo),
-                ),
-              ),
-              elevation: 8,
-              shadowColor: Colors.red,
-              margin: const EdgeInsets.all(20),
-              shape: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Colors.red, width: 2)),
-               ),//Card 3
-          
-          ]) //Columna
-       child: ElevatedButton(
+      body: Column(children: [
+        Card(
+          child: const ListTile(
+            leading: Icon(Icons.people),
+            title: Text(
+              "JUGADORES",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          elevation: 8,
+          shadowColor: Colors.red,
+          margin: const EdgeInsets.all(20),
+          shape: OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: const BorderSide(color: Colors.red, width: 2)),
+        ), //Card1
+        Card(
+          child: const ListTile(
+            leading: Icon(Icons.people),
+            title: Text(
+              "QUINIELA",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          elevation: 8,
+          shadowColor: Colors.red,
+          margin: const EdgeInsets.all(20),
+          shape: OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: const BorderSide(color: Colors.red, width: 2)),
+        ), //Card2
+        Card(
+          child: const ListTile(
+            leading: Icon(Icons.people),
+            title: Text(
+              "NOTICIAS",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          elevation: 8,
+          shadowColor: Colors.red,
+          margin: const EdgeInsets.all(20),
+          shape: OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: const BorderSide(color: Colors.red, width: 2)),
+        ), //Card 3
+        Card(
+          child: const ListTile(
+            leading: Icon(Icons.people),
+            title: Text(
+              "EQUIPOS",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          elevation: 8,
+          shadowColor: Colors.red,
+          margin: const EdgeInsets.all(20),
+          shape: OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: const BorderSide(color: Colors.red, width: 2)),
+        ), //Card 4
+        Card(
+          child: const ListTile(
+            leading: Icon(Icons.people),
+            title: Text(
+              "PROG.PARTIDOS",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          elevation: 8,
+          shadowColor: Colors.red,
+          margin: const EdgeInsets.all(20),
+          shape: OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: const BorderSide(color: Colors.red, width: 2)),
+        ), //Card 5
+        ElevatedButton(
           // Within the segundaScreen widget
           onPressed: () {
             // Navigate back to the first screen by popping the current route
             // off the stack.
             Navigator.pop(context);
           },
-          child: const Text('Go back!'),
+          child: const Text('Regresar a la primera pantalla'),
+          style: ElevatedButton.styleFrom(
+            onPrimary: Colors.white,
+            primary: Colors.purple,
+            onSurface: Colors.grey,
+            side: BorderSide(color: Colors.black, width: 1),
+            elevation: 20,
+            minimumSize: Size(150, 50),
+            shadowColor: Colors.teal,
+            shape: BeveledRectangleBorder(side: BorderSide(color: Colors.green, width: 2), borderRadius: BorderRadius.circular(15)),
+          ),
         ),
-      ), //body center
+      ]), //Columna
+
+      //body center
     ); //scaffold
   } //widget
 } //Pantalla 2
